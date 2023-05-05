5ffde259f44172988f34448d0a024a182c65fcd1
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-05 09:55:17 +0200

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import "@/styles/globals.scss";
 import { useState } from "react";
 import Footer from "./components/Footer";
+import { DefaultSeo } from "next-seo";
+import relImage from "../../public/K.png";
 
 export default function App({ Component, pageProps }) {
   const [theme, setTheme] = useState("light");
@@ -16,6 +18,29 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <DefaultSeo
+        title="Kristof’s Journal"
+        description="My ideas and thoughts about web development, programming and self
+            improvement"
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://www.url.ie/",
+          siteName: "Kristof’s Journal",
+        }}
+        twitter={{
+          handle: "@kristf_nemeth",
+          site: "myawesomewebsite.com",
+          cardType: "summary",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            type: "image/png",
+            href: `${relImage.src}`,
+          },
+        ]}
+      />
       <Header theme={theme} toggleTheme={toggleTheme} />
       <Component {...pageProps} theme={theme} />
       <Footer theme={theme} />
