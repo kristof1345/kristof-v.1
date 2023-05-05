4bf4ba93bc557b7f19b149f91337a90152cbc322
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-05 10:24:21 +0200

    apos2

--- src/pages/articles/the-struggles-of-becoming-a-self-taught-developer.js
+++ src/pages/articles/the-struggles-of-becoming-a-self-taught-developer.js
@@ -28,17 +28,17 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
             <h3 className={`${theme === "light" ? "lightlogo" : "darklogo"}`}>
               Want to become a web developer?
             </h3>
-            <p>Cool, you don't need a degree for that.</p>
+            <p>Cool, you don&apos;t need a degree for that.</p>
             <p>
-              You don't even need to go through a boot camp or pay for an
+              You don&apos;t even need to go through a boot camp or pay for an
               expensive course.
             </p>
             <p>
               You can learn web development by yourself. This is known as a
               self-taught developer. While it can be a rewarding experience, it
-              also comes with its own set of challenges. But if this doesn't
-              scare you off. Let's see a couple of the problems self-taught
-              developers face.
+              also comes with its own set of challenges. But if this
+              doesn&apos;t scare you off. Let&apos;s see a couple of the
+              problems self-taught developers face.
             </p>
             <div>
               <h2 className={`${theme === "light" ? "lightlogo" : "darklogo"}`}>
@@ -53,8 +53,8 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
               <p>
                 Afterall, the decision is yours to make, as you know yourself
                 best. But, I can offer one piece of advice: no matter which
-                route you choose, stick with it. Don't jump from one course to
-                the next halfway through. I know from experience that this is
+                route you choose, stick with it. Don&apos;t jump from one course
+                to the next halfway through. I know from experience that this is
                 not a productive approach. Instead, commit to the path you have
                 chosen and stick with it through to the end.
               </p>
@@ -119,9 +119,9 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
               </h2>
               <p>
                 Loneliness is one of the biggest demons of a self-taught
-                developer. Having friends is great. But if you can't share your
-                journey with someone, it can be difficult to stay motivated and
-                inspired.
+                developer. Having friends is great. But if you can&apos;t share
+                your journey with someone, it can be difficult to stay motivated
+                and inspired.
               </p>
               <p>
                 Of course, it is possible to be a lonely web developer. But it
@@ -129,12 +129,12 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
                 share it with.
               </p>
               <p>
-                If you don't have someone in your life who does the same thing
-                you do, don't worry - you are not alone. There are thousands of
-                lonely developers out there. My advice would be to join a
-                community, whether it's online or in person. Doing so can be a
-                great experience and can open up a wealth of knowledge and
-                support.
+                If you don&apos;t have someone in your life who does the same
+                thing you do, don&apos;t worry - you are not alone. There are
+                thousands of lonely developers out there. My advice would be to
+                join a community, whether it&apos;s online or in person. Doing
+                so can be a great experience and can open up a wealth of
+                knowledge and support.
               </p>
             </div>
             <div>
@@ -144,21 +144,22 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
               <p>
                 When it comes to learning something on your own, the key is to
                 create a plan and then turn it into a habit. This way, it will
-                become almost automatic and you won't have to think about it -
-                you'll just do it.
+                become almost automatic and you won&apos;t have to think about
+                it - you&apos;ll just do it.
               </p>
               <p>
-                If you're learning to code, you're likely juggling other
-                responsibilities too. So how do you find the time to code every
-                day? If you're having trouble finding time, it's likely because
-                you're not dedicated enough. But let's say you can make the time
-                - the next question is how much time should you dedicate to
-                coding daily? The answer to this depends on you and your goals,
-                but dedicating 1-2 hours a day is more than enough.
+                If you&apos;re learning to code, you&apos;re likely juggling
+                other responsibilities too. So how do you find the time to code
+                every day? If you&apos;re having trouble finding time, it&apos;s
+                likely because you&apos;re not dedicated enough. But let&apos;s
+                say you can make the time - the next question is how much time
+                should you dedicate to coding daily? The answer to this depends
+                on you and your goals, but dedicating 1-2 hours a day is more
+                than enough.
               </p>
               <p>
-                Remember, it's not about who does the most work in a day, it's
-                about who is the most consistent.
+                Remember, it&apos;s not about who does the most work in a day,
+                it&apos;s about who is the most consistent.
               </p>
             </div>
             <div>
@@ -172,17 +173,17 @@ const theStugglesOfBecomingASelfTaughtDeveloper = ({ theme }) => {
               <p>
                 Tutorial hell looks something like this: You start out learning
                 from a course. You watch the course and copy the code, feeling
-                amazing that you've learned something new. But when you try to
-                recall it the next day, you realize you've forgotten it and have
-                to start from the beginning again. When you're stuck in this
-                never-ending cycle, you might feel like you're learning to code.
-                But that is an illusion. When you want to build something
-                without a tutorial, you get stuck.
+                amazing that you&apos;ve learned something new. But when you try
+                to recall it the next day, you realize you&apos;ve forgotten it
+                and have to start from the beginning again. When you&apos;re
+                stuck in this never-ending cycle, you might feel like
+                you&apos;re learning to code. But that is an illusion. When you
+                want to build something without a tutorial, you get stuck.
               </p>
               <p>
-                My suggestion to you is to take notes. That's right - take a
-                course and take notes. When you finish a lesson, review the
-                lesson notes and I guarantee you'll remember way more than
+                My suggestion to you is to take notes. That&apos;s right - take
+                a course and take notes. When you finish a lesson, review the
+                lesson notes and I guarantee you&apos;ll remember way more than
                 before.
               </p>
             </div>
