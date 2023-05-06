2a9df317f5583e5451792569d14398503a28c405
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-06 15:19:21 +0200

    clarity

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Image from "next/image";
 
 export default function Document() {
   return (
@@ -8,32 +7,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/93490260"
-              style={{ position: "absolute", left: "-9999px" }}
-              width={5}
-              height={5}
-              alt=""
-            />
-          </div>
-        </noscript>
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(93490260, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gzqez30rhy");
           `,
           }}
         />
